bfed0e0ec86ea60fb818be28796be56f96286d3f
JasonRavager <vergara.ecjr@gmail.com>
2024-06-15 00:06:57 +0800

    finish contact form feature.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const { ObjectId } = require("mongodb");
 const sanitizeHtml = require("sanitize-html");
 const petsCollection = require("../db").db().collection("pets");
+const contactsCollection = require("../db").db().collection("contacts");
 
 
 const sanitizeOptions = {
@@ -16,6 +17,18 @@ exports.submitContact = async function(req, res, next) {
     return res.json({ message: "Sorry!" });
   };
 
+  if (typeof req.body.name != "string") {
+    req.body.name = "";
+  };
+
+  if (typeof req.body.email != "string") {
+    req.body.email = "";
+  };
+
+  if (typeof req.body.comment != "string") {
+    req.body.comment = "";
+  };
+
   if (!validator.isEmail(req.body.email)) {
     console.log("invalid email detected");
     return res.json({ message: "Sorry!" });
@@ -26,7 +39,8 @@ exports.submitContact = async function(req, res, next) {
     return res.json({ message: "Sorry!" });
   };
 
-  const doesPetExist = await petsCollection.findOne({ _id: new ObjectId(req.body.petId)});
+  req.body.petId = new ObjectId(req.body.petId);
+  const doesPetExist = await petsCollection.findOne({ _id: req.body.petId });
 
   if (!doesPetExist) {
     console.log("pet does not exist");
@@ -34,6 +48,7 @@ exports.submitContact = async function(req, res, next) {
   };
 
   const ourObject = {
+    petId: req.body.petId,
     name: sanitizeHtml(req.body.name, sanitizeOptions),
     email: sanitizeHtml(req.body.email, sanitizeOptions),
     comment: sanitizeHtml(req.body.comment, sanitizeOptions),
@@ -72,10 +87,33 @@ exports.submitContact = async function(req, res, next) {
       </p>`
     });
 
-    await Promise.all([promise1, promise2]);
+    const promise3 = await contactsCollection.insertOne(ourObject);
+
+    await Promise.all([promise1, promise2, promise3]);
   } catch (err) {
     next(err);
   }
 
   res.send("Thanks for sending data to us.");
+};
+
+exports.viewPetContacts = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    console.log("bad id");
+    return res.redirect("/");
+  };
+
+  const pet = await petsCollection.findOne({
+    _id: new ObjectId(req.params.id)
+  });
+
+  if (!pet) {
+    console.log("pet does not exist");
+    return res.redirect("/");
+  };
+
+  const contacts = await contactsCollection.find({
+    petId: new ObjectId(req.params.id)
+  }).toArray();
+  res.render("pet-contacts", { contacts, pet });
 };
